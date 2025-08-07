d8753fb90b2ebf56c6fc0118f3749f4288cb4e05
Dunaka Chetan <chetan521230@gmail.com>
2025-08-07 12:18:16 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,11 +30,4 @@ public class AppController {
 	public String update(@RequestBody Product p) {
 		return s.updateData(p);
 	}
-
-	/* 
-	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable int id) {
-		return s.deleteData(id);
-	}
-	*/
 }
